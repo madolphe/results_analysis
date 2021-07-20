40ff16a8b77ea9383a8fa05e0454b6ef79144aa4
masataka sawayama <masa.sawayama@gmail.com>
2021-07-20 22:55:54 +0200

    Add Bayesian analysis to gonogo task

--- results-analysis/gonogo_ana_results.py
+++ results-analysis/gonogo_ana_results.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from cal_stan_accuracy_rt import CalStan_accuracy, CalStan_rt
 
 
 def transform_str_to_list(row, columns):
@@ -113,7 +114,11 @@ def compute_means(row: pd.core.series.Series) -> float:
     """
         Useless function that returns the mean on a row (pandas already provides one)
     """
-    return np.mean(row['result_clean_rt'])
+    tmp = np.array(row['result_clean_rt'])
+    #this was changed to calculate only the hit trials by mswym
+    #print(np.mean(tmp[np.where(tmp!=0)]))
+    #print(np.mean(row['result_clean_rt']))
+    return np.mean(tmp[np.where(tmp!=0)])
 
 
 def compute_number_of_omissions(row: pd.core.series.Series) -> int:
@@ -129,6 +134,76 @@ def compute_number_of_omissions(row: pd.core.series.Series) -> int:
                 count += 1
     return count
 
+def compute_result_sum_hr(row):
+    return 18-row['result_nb_omission']
+
+def compute_numbercond(row,ind_cond):
+    """
+        From the row of results, return the list of resuluts_correct according to results_num_stim condition 
+    """
+    results_responses = list(row["result_correct"])
+    results_targetvalue = [int(t) for t in row["results_targetvalue"].split(',')]
+    out = [results_responses[ind] for ind,t in enumerate(results_targetvalue) if t==ind_cond]
+    return np.array(out)
+
+def compute_sum_to_row(row,column):
+    return np.sum(row[column])
+
+def extract_id(dataframe,num_count):
+    mask = pd.DataFrame(dataframe.participant_id.value_counts() == num_count)
+    indices_id = mask[mask['participant_id'] == True].index.tolist()
+    return indices_id
+
+def extract_mu_ci_from_summary_accuracy(dataframe,ind_cond):
+    out = np.zeros((len(ind_cond),3)) #3 means the mu, ci_min, and ci_max
+    for t,ind in enumerate(ind_cond):
+        out[t,0] = dataframe[ind].mu_theta
+        out[t,1] = dataframe[ind].ci_min
+        out[t,2] = dataframe[ind].ci_max
+    return out
+
+def draw_all_distributions(dist_ind,dist_summary,num_dist,num_cond,std_val = 0.05,fname_save='workingmemory_accuracy.png'):
+    # dist_ind is the matrix of (num_observers x num_conditions)
+    # dist_summary is the mu (numb_conditions), ci_min, and ci_max
+    x = [j+1+std_val*np.random.randn() for j in range(num_cond) for t in range(num_dist)]
+    dist_ind = dist_ind.T.flatten()
+    x_sum = np.linspace(1,num_cond,num_cond)
+    fig = plt.figure(figsize=(5,5))
+    axes = fig.add_subplot(1,1,1)
+    axes.scatter(x,dist_ind,s=10,c='blue')
+    axes.errorbar(x_sum, dist_summary[:,0], yerr = [dist_summary[:,0]-dist_summary[:,1], dist_summary[:,2]-dist_summary[:,0]], capsize=5, fmt='o', markersize=15, ecolor='red', markeredgecolor = "red", color='w')
+    axes.set_xticks([1,2])
+    axes.set_xlim([0.5,2.5])
+    axes.set_xticklabels(['HR','FAR'],fontsize=20)
+    axes.set_yticks([0,0.2,0.4,0.6,0.8,1.0])
+    axes.set_yticklabels(['0.0','0.2','0.4','0.6','0.8','1.0'],fontsize=20)
+    fig.savefig(fname_save)
+    plt.show()
+
+def draw_all_distributions_rt(dist_ind,dist_summary,num_dist,num_cond,std_val = 0.05,fname_save='memorability_rt.png'):
+    # dist_ind is the matrix of (num_observers x num_conditions)
+    # dist_summary is the mu (numb_conditions), ci_min, and ci_max
+    x = [j+1+std_val*np.random.randn() for j in range(num_cond) for t in range(num_dist)]
+    dist_ind = dist_ind.T.flatten()
+    x_sum = np.linspace(1,num_cond,num_cond)
+    fig = plt.figure(figsize=(5,5))
+    axes = fig.add_subplot(1,1,1)
+    axes.scatter(x,dist_ind,s=10,c='blue')
+    axes.errorbar(x_sum, dist_summary[:,0], yerr = [dist_summary[:,0]-dist_summary[:,1], dist_summary[:,2]-dist_summary[:,0]], capsize=5, fmt='o', markersize=15, ecolor='red', markeredgecolor = "red", color='w')
+    axes.set_xlim([0.5,1.5])
+    axes.set_xticks([1])
+    axes.set_xticklabels(['HR'],fontsize=20)
+    axes.set_yticks([0,250,500,750])
+    axes.set_yticklabels(['0','250','500','750'],fontsize=20)
+    fig.savefig(fname_save)
+    plt.show()
+
+def extract_mu_ci_from_summary_rt(dataframe):
+    out = np.zeros((1,3)) #3 means the mu, ci_min, and ci_max
+    out[0,0] = dataframe.mu_rt
+    out[0,1] = dataframe.ci_min
+    out[0,2] = dataframe.ci_max
+    return out
 
 if __name__ == '__main__':
     csv_path = "../outputs/gonogo/gonogo.csv"
@@ -185,3 +260,42 @@ if __name__ == '__main__':
 
     # Save data
     dataframe.to_csv("../outputs/gonogo/gonogo_treatment.csv")
+
+
+    #from here written by mswym
+    #condition extraction
+    dataframe['result_correct'] = dataframe.apply(compute_result_sum_hr, axis=1)
+    #dataframe['result_nb_omission']
+
+    #extract observer index information
+    indices_id = extract_id(dataframe,num_count=2)
+    
+    #sumirize two days experiments
+    sum_observers = []
+    for ob in indices_id:
+        print(ob)
+        tmp_df = dataframe.groupby(["participant_id"]).get_group(ob)
+        sum_observers.append([np.sum(tmp_df.result_correct),np.sum(tmp_df.result_nb_omission),np.mean(tmp_df.mean_result_clean_rt)])
+        
+    sum_observers = pd.DataFrame(sum_observers)
+    #for save summary data
+    tmp = sum_observers/36.
+    tmp.loc[:,2] = tmp.loc[:,2]*36
+    tmp.to_csv('../outputs/gonogo/sumdata_egonogo.csv',header=False, index=False)
+    sum_observers['total_resp'] = sum_observers.apply(lambda row: 36, axis=1) #two days task
+
+    #calculate the mean distribution and the credible interval
+    class_stan_accuracy = [CalStan_accuracy(sum_observers,ind_corr_resp=n) for n in range(2)]
+    class_stan_rt = CalStan_rt(sum_observers,ind_rt=2,max_rt = 1000)
+
+    #draw figures
+    #for accuracy data
+    dist_ind = sum_observers.iloc[0:len(sum_observers),0:2].values/36.
+    dist_summary = extract_mu_ci_from_summary_accuracy(class_stan_accuracy,[0,1])
+    draw_all_distributions(dist_ind,dist_summary,len(sum_observers),num_cond=2,std_val = 0.05,fname_save='../outputs/gonogo/gonogo_hrfar.png')
+    
+
+    dist_ind = sum_observers.iloc[0:len(sum_observers),2].values
+    dist_summary = extract_mu_ci_from_summary_rt(class_stan_rt)
+    draw_all_distributions_rt(dist_ind,dist_summary,len(sum_observers),num_cond=1,std_val = 0.05,fname_save='../outputs/gonogo/gonogo_rt.png')
+    print('finished')
