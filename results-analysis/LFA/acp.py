963926dfe81ab09e30455305b5ef393b8e788966
masataka sawayama <masa.sawayama@gmail.com>
2021-08-23 17:28:28 +0200

    Fix latent factor analysis

--- results-analysis/LFA/acp.py
+++ results-analysis/LFA/acp.py
@@ -1,23 +1,41 @@
 import pandas as pd
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, FastICA,DictionaryLearning
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.stats import norm
 
 # Delete FA for memorability
 # Delete RT for all
 # Plot RDM
 
+def inv_norm_transform(dataframe):
+    #first 0 and 1 number is replaced according to the total task number to avoid infinity. See also Macmillan, N. A., & Kaplan, H. L. (1985).
+    task_names = ['moteval', 'loadblindness', 'memorability', 'enumeration', 'workingmemory']
+    num_totals = [15.,20.,16.,20.,12.]
+    for col in dataframe.columns:
+        for task,num in zip(task_names,num_totals):
+            if task in col:
+                dataframe[col] = dataframe[col].mask(dataframe[col]==1,1-(1/(2*num)))
+                dataframe[col] = dataframe[col].mask(dataframe[col]==0,1/(2*num))
+        #invert normal transformation is applyed only for the accuracy data, not for rt. For rt data, I apply log transform.
+        if 'rt' not in col:
+            dataframe[col] = dataframe[col].apply(norm.ppf)
+        else:
+            dataframe[col] = dataframe[col].apply(np.log)
+    return dataframe
+
 if __name__ == '__main__':
-    pca = PCA(6)
-    dataframe = pd.read_csv('tmp_merge_acp_all.csv')
+    pca = PCA(10)
+    dataframe = pd.read_csv('tmp_merge_acp_all_plus_rt.csv')
     dataframe = dataframe.fillna(dataframe.mean())
     dataframe.drop(columns=['Unnamed: 0', "task_status", 'participant_id'], inplace=True)
     # dataframe = dataframe[dataframe.columns[:-20]]
+    dataframe = inv_norm_transform(dataframe)
     normalized_dataframe = (dataframe - dataframe.mean()) / dataframe.std()
     cov_matrix = normalized_dataframe.cov()
-    pca.fit(dataframe.values)
+    pca.fit(normalized_dataframe.values)
     plt.scatter([i for i in range(1, len(pca.explained_variance_ratio_) + 1)], np.cumsum(pca.explained_variance_ratio_))
     plt.xlabel('Number of components')
     plt.xticks(np.arange(1, len(pca.explained_variance_ratio_) + 1),
@@ -30,7 +48,8 @@ if __name__ == '__main__':
     results_loading.to_csv('loadings.csv')
 
     plt.figure()
-    sns.heatmap(results_loading.abs().values)
+    #sns.heatmap(results_loading.abs().values)
+    sns.heatmap(results_loading.values,cmap="PuOr",center=0)
     plt.savefig('heatmap_loadings.png')
     plt.close()
 
@@ -49,3 +68,24 @@ if __name__ == '__main__':
     plt.close()
     print(pca.explained_variance_ratio_)
     print(pca.singular_values_)
+
+
+    ica = FastICA(10)
+    ica.fit(normalized_dataframe.values)
+    Uica = ica.mixing_ 
+    Aica = ica.transform(normalized_dataframe.values).T
+    plt.figure()
+    sns.heatmap(Uica,cmap="PuOr",center=0)
+    plt.savefig('heatmap_loadings_ica.png')
+    plt.close()
+
+    sparse = DictionaryLearning(10)
+    sparse.fit(normalized_dataframe.values)
+    Usc = sparse.components_ .T
+    Asc = sparse.transform(normalized_dataframe.values).T
+    plt.figure()
+    sns.heatmap(Usc,cmap="PuOr",center=0)
+    plt.savefig('heatmap_loadings_sparse.png')
+    plt.close()
+    import pdb;pdb.set_trace()
+    a =1
